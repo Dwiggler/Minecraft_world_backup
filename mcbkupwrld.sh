685e3bc75bbbf2ee7df15cef3b9531ca700b3079
Dwiggles <tanacioustwinkie@live.com>
2023-12-25 20:26:28 -0600

    removed / on world directory

--- mcbkupwrld.sh
+++ mcbkupwrld.sh
@@ -3,7 +3,7 @@
 #get world directory
 mcunit=$(systemctl status minecraft.service | grep "/system/" | awk -F '(' '{print $2}' | awk -F ';' '{print $1}')
 workingdirectory=$(grep WorkingDirectory /etc/systemd/system/minecraft.service | awk -F '=' '{print $2}')
-worlddirectory=$($workingdirectory"/world")
+worlddirectory=$($workingdirectory"world")
 echo $worlddirectory > /tmp/bkup_test.txt
 
 
