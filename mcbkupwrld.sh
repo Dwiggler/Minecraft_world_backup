0715b3d5951e206bfee39baf6361a68cc86180a1
Dwiggles <tanacioustwinkie@live.com>
2024-01-01 22:52:10 -0600

    fleshed out vars and main func's. Still need to create old file deletions and then test script.

--- mcbkupwrld.sh
+++ mcbkupwrld.sh
@@ -1,22 +1,37 @@
 #!/bin/sh
 
-#get world directory
+#set veriables
+bkupdirect=/opt/minecraft/mainsurvival/wrldbkup
 mcunit=$(systemctl status minecraft.service | grep "/system/" | awk -F '(' '{print $2}' | awk -F ';' '{print $1}')
-workingdirectory=$(grep WorkingDirectory /etc/systemd/system/minecraft.service | awk -F '=' '{print $2}')
-worlddirectory=$($workingdirectory"world")
-echo $worlddirectory > /tmp/bkup_test.txt
+wrkingdirect=$(grep WorkingDirectory /etc/systemd/system/minecraft.service | awk -F '=' '{print $2}')
+worlddirect=$($wrkingdirect"world")
+#echo $worlddirect > /tmp/bkup_test.txt
 
+#does wrldbkup directory exist? If not, create it.
+if [ ! -d "$bkupdirect" ]; then {
+  mkdir /opt/minecraft/mainsurvival/wrldbkup
+}
+fi
+
+service_check () {
+        ps -ef | grep mainsurvival | grep -v grep | wc -l
+}
 
 #Stop the minecraft service
-
+systemctl stop minecraft
+x=1
+while [ $x != 0 ]; do	x=$(service_check); done
 
 #create date/timestamp
+timestamp=$(date +%Y%m%d%H%M)
 
 #Copy file to backup directory
+cp -R /opt/minecraft/mainsurvival/world /opt/minecraft/mainsurvival/wrldbkup/mainsurvivalbkup
 
 #Restart Minecraft
+systemctl start minecraft
 
-#Change name of world file to date/timestamp for version
+#tar file and set to name of world and add timestamp
+tar -czf mainsurvival"$timestamp".tar.gz /opt/minecraft/mainsurvival/wrldbkup/mainsurvivalbkup
 
 #Delete 4th oldest file
-exit
