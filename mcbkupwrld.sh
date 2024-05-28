545acc008a115213c95ff8d130137fe0e2d086ff
Dwiggles <tanacioustwinkie@live.com>
2024-05-27 19:56:43 -0500

    Edited variables and added file rotation. Also has diff for bkup compl.

--- mcbkupwrld.sh
+++ mcbkupwrld.sh
@@ -1,18 +1,34 @@
 #!/bin/sh
 
 #set veriables
-bkupdirect=/opt/minecraft/mainsurvival/wrldbkup
+bkupdirect=/opt/minecraft/wrldbkups
 mcunit=$(systemctl status minecraft.service | grep "/system/" | awk -F '(' '{print $2}' | awk -F ';' '{print $1}')
-wrkingdirect=$(grep WorkingDirectory /etc/systemd/system/minecraft.service | awk -F '=' '{print $2}')
-worlddirect=$($wrkingdirect"world")
+wrkingdirect=$(grep WorkingDirectory $mcunit | awk -F '=' '{print $2}')
+worlddirect=$($wrkingdirect/"world")
 #echo $worlddirect > /tmp/bkup_test.txt
 
 #does wrldbkup directory exist? If not, create it.
 if [ ! -d "$bkupdirect" ]; then {
-  mkdir /opt/minecraft/mainsurvival/wrldbkup
+  mkdir /opt/minecraft/wrldbkups
 }
 fi
 
+#Delete 4th oldest file confirm in directory when WorkingDirectory
+cd /opt/minecraft/mainsurvival/wrldbkup
+if [ -e *_3_*.tar.gz ]; then  {
+  rm -f *_3_*.tar.gz
+}
+fi
+if [ -e *_2_*.tar.gz ]; then  {
+  rename _2_ _3_ *_2_*.tar.gz
+}
+fi
+if [ -e *_1_*.tar.gz ]; then  {
+  rename _1_ _2_ *_1_*.tar.gz
+}
+fi
+
+#function for seeing if service is running
 service_check () {
         ps -ef | grep mainsurvival | grep -v grep | wc -l
 }
@@ -28,10 +44,22 @@ timestamp=$(date +%Y%m%d%H%M)
 #Copy file to backup directory
 cp -R /opt/minecraft/mainsurvival/world /opt/minecraft/mainsurvival/wrldbkup/mainsurvivalbkup
 
+#create temp files with both directories to do comparisons with diff to know when backup is complete
+i=1
+while [$i != 0 ]
+do
+  cd /opt/minecraft/mainsurvival/world
+  du -hs * > /tmp/pre.txt
+  cd /opt/minecraft/mainsurvival/wrldbkup/mainsurvivalbkup
+  du -hs * > /tmp/post.txt
+
+  i=$(diff /tmp/pre.txt /tmp/post.txt | wc -l);
+done
+
+rm -f /tmp/pre.txt /tmp/post.txt
+
 #Restart Minecraft
 systemctl start minecraft
 
 #tar file and set to name of world and add timestamp
-tar -czf mainsurvival"$timestamp".tar.gz /opt/minecraft/mainsurvival/wrldbkup/mainsurvivalbkup
-
-#Delete 4th oldest file
+tar -czf mainsurvival_1_"$timestamp".tar.gz /opt/minecraft/mainsurvival/wrldbkup/mainsurvivalbkup
